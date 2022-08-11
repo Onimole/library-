62f36a78c8ab3ee0a619e31693340874da4c5108
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-11 01:15:27 +0100

    add the views

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,8 @@
+from msilib.schema import ListView
 from django.shortcuts import render
-
+from .models import Books
 # Create your views here.
+
+class BookListView(ListView):
+    model = Books
+    template_name = 'book_list.html'
