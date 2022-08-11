1311b62d9f69dda404a01cc780be4c36b0f6ea00
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-11 01:14:18 +0100

    add the class book list view

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,15 @@
+from distutils.text_file import TextFile
+from pyexpat import model
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length= 150)
+    subtitle = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title, self.author
