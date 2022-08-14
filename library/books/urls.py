c5cd24db4bb4ed717584dae7bdde0cfc75004744
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-14 23:30:12 +0100

    add url file to books

---
+++ library/books/urls.py
@@ -0,0 +1,10 @@
+import imp
+from operator import imod
+from django.urls import path
+
+from books.models import Book
+from .views import BookListView
+
+urlpatterns = [
+    path('', BookListView.as_view(), name = 'home'),
+]
