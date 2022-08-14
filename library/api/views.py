0d118673ff625d7af8d1522a766419e7e632f610
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-14 23:22:32 +0100

    update the api views add the serialize class

--- library/api/views.py
+++ library/api/views.py
@@ -1,3 +1,9 @@
+import imp
 from django.shortcuts import render
+from rest_framework import generics
+from books.models import Book
+from .serializer import BookSerializer
 
-# Create your views here.
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
