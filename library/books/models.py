a691e6a020aa3b4eb2f2d030e8db56783debee86
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-14 23:25:51 +0100

    update book model file

--- library/books/models.py
+++ library/books/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length= 150)
     subtitle = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
